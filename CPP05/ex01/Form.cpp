d8e2ef54784fbfe8208b88d68ee5e1da9444aa3a
Stan Desseaux le Beau <sdesseau@josmac.home>
2022-10-24 19:31:07 +0200

    reglages a la con signForm

--- CPP05/ex01/Form.cpp
+++ CPP05/ex01/Form.cpp
@@ -6,7 +6,7 @@
 /*   By: sdesseau <sdesseau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 13:47:56 by sdesseau          #+#    #+#             */
-/*   Updated: 2022/10/24 19:15:13 by sdesseau         ###   ########.fr       */
+/*   Updated: 2022/10/24 19:30:58 by sdesseau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,9 @@ bool	Form::getSigned() const { return (this->_signed); }
 
 void Form::beSigned(Bureaucrat const& bureaucrat)
 {
-	if (bureaucrat.getGrade() <= this->getSignedGrade())
+	if (this->signed == true)
+		throw std::invalid_argument("Form::AlreadySigned");
+	else if (bureaucrat.getGrade() <= this->getSignedGrade())
 		this->_signed = true;
 	else
 		throw std::invalid_argument("Form::GradeTooLowException");
